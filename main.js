1a96ffddfb0d8ab9eeedb6fbdbc2c1be456921c5
Chris Branson <chrisbranson@live.co.uk>
2023-03-16 15:41:05 +0000

    Task one complete

---
+++ main.js
@@ -0,0 +1,20 @@
+const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"]
+
+console.log(favouriteSongs)
+
+const Profit = [1, 2, 4, 8, 16, 32, 64]
+console.log(Profit)
+
+const friends = [
+    {Name: `Chris`, Skill: `Changing lives`},
+    {Name: `Lizzie`, Skill: `Being friendly`},
+    {Name: `Ben`, Skill: `Code rapping`},
+    {Name: `Tao`, Skill: `Hand raising`},
+    {Name: `Liz`, Skill: `Crochet`},
+    {Name: `Patrick`, Skill: `Call of Duty`},
+    {Name: `Tim`, Skill: `Analogies`},
+    {Name: `James`, Skill: `Friendship`},
+    {Name: `Joseph`, Skill: `Being positive`},
+    {Name: `Max`, Skill: `Paper-mache`},
+]
+console.log(friends)
